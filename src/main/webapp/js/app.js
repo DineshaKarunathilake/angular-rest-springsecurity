831916ccd2bf738944ee3588b277d7cd982d7018
DineshaKarunathilake <dineshakarunathilake@gmail.com>
2016-10-07 12:44:50 +0530

    displaying size, customer

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,4 @@
-angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services'])
+angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services','xeditable'])
 	.config(
 		[ '$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
 			
@@ -19,14 +19,28 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services'])
 			$routeProvider.when('/login', {
 				templateUrl: 'partials/login.html',
 				controller: LoginController
+
 			});
 			
+
+//			$routeProvider.otherwise(
+//            			{
+//                            templateUrl: 'partials/viewmeasurements.html',
+//                        	controller: ViewMeasurementsController
+//                        });
+            $routeProvider.when('/add', {
+                            templateUrl: 'partials/add.html',
+                            controller: EditableTableCtrl
+
+                        });
+
 			$routeProvider.otherwise(
-			{
-                templateUrl: 'partials/viewmeasurements.html',
-            	controller: ViewMeasurementsController
-            });
-			
+            			{
+                            templateUrl: 'partials/viewmeasurements.html',
+                        	controller: ViewMeasurementsController
+                        });
+
+
 			$locationProvider.hashPrefix('!');
 			
 			/* Register error provider that shows message on failed requests or redirects to login page on
@@ -73,7 +87,9 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services'])
 		   
 		} ]
 		
-	).run(function($rootScope, $location, $cookieStore, UserService) {
+	).run(function($rootScope, $location, $cookieStore, UserService,editableOptions) {
+
+	     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 		
 		/* Reset error when a new view is loaded */
 		$rootScope.$on('$viewContentLoaded', function() {
@@ -116,6 +132,7 @@ angular.module('exampleApp', ['ngRoute', 'ngCookies', 'exampleApp.services'])
 	});
 
 
+
 function IndexController($scope, BlogPostService) {
 
 	$scope.blogPosts = BlogPostService.query();
@@ -127,10 +144,19 @@ function IndexController($scope, BlogPostService) {
 	};
 }
 
+
 function NewMeasurementController($scope,$routeParams, $location, NewMeasurementService) {
 
 	$scope.newMeasurementEntry = new NewMeasurementService();
 	$scope.newMeasurementEntry = NewMeasurementService.get({id: $routeParams.id});
+
+	 $scope.sizes = [
+        {id: 1, s: 'XS'},
+        {id: 2, s: 'S'},
+        {id: 3, s: 'M'},
+        {id: 4, s: 'L'},
+        {id: 5, s: 'XL'}
+      ];
 
 	$scope.save = function() {
 		$scope.newMeasurementEntry.$save(function() {
@@ -192,7 +218,125 @@ function LoginController($scope, $rootScope, $location, $cookieStore, UserServic
 			});
 		});
 	};
-};
+	};
+
+function EditableTableCtrl($scope, $filter, $http, $q){
+ $scope.entries = [
+    {id: 1, gmt: 'Garment1'},
+    {id: 2, gmt: 'Garment2'},
+    {id: 3, gmt: 'Garment3'},
+    {id: 4, gmt: 'Garment4'},
+    {id: 5, gmt: 'Garment5'}
+  ];
+
+  $scope.statuses = [
+    {value: 1, text: 'status1'},
+    {value: 2, text: 'status2'},
+    {value: 3, text: 'status3'},
+    {value: 4, text: 'status4'}
+  ];
+
+//  $scope.measurements = [
+//    {value: 1,text: 'Chest Width'},
+//    {value: 2,text: 'Hem Width'},
+//    {value: 3,text: 'CB Length'},
+//    {value: 4,text: 'CF Length'},
+//    {value: 5,text: 'Sleeve Opening'},
+//    {value: 1,text: 'Sleeve Length'},
+//    {value: 1,text: 'Sleeve Width'},
+//  ];
+
+
+  $scope.groups = [];
+  $scope.loadGroups = function() {
+    return $scope.groups.length ? null : $http.get('/groups').success(function(data) {
+      $scope.groups = data;
+    });
+  };
+
+  $scope.showGroup = function(entry) {
+    if(entry.group && $scope.groups.length) {
+      var selected = $filter('filter')($scope.groups, {id: entry.group});
+      return selected.length ? selected[0].text : 'Not set';
+    } else {
+      return entry.groupName || 'Not set';
+    }
+  };
+
+  $scope.showStatus = function(entry) {
+    var selected = [];
+    if(entry.status) {
+      selected = $filter('filter')($scope.statuses, {value: entry.status});
+    }
+    return selected.length ? selected[0].text : 'Not set';
+  };
+
+//  $scope.checkName = function(data, id) {
+//    if (id === 2 && data !== 'awesome') {
+//      return "Username 2 should be `awesome`";
+//    }
+//  };
+
+  // filter users to show
+  $scope.filterEntry = function(entry) {
+    return entry.isDeleted !== true;
+  };
+
+  // mark user as deleted
+  $scope.deleteEntry = function(id) {
+    var filtered = $filter('filter')($scope.entry, {id: id});
+    if (filtered.length) {
+      filtered[0].isDeleted = true;
+    }
+  };
+
+  // add user
+  $scope.addEntry = function() {
+    $scope.entries.push({
+      id: $scope.entries.length+1,
+      measurement: '',
+      status: null,
+      group: null,
+      isNew: true
+    });
+  };
+
+  // cancel all changes
+  $scope.cancel = function() {
+    for (var i = $scope.entries.length; i--;) {
+      var user = $scope.entries[i];
+      // undelete
+      if (entry.isDeleted) {
+        delete entry.isDeleted;
+      }
+      // remove new
+      if (entry.isNew) {
+        $scope.entries.splice(i, 1);
+      }
+    };
+  };
+
+  // save edits
+  $scope.saveTable = function() {
+    var results = [];
+    for (var i = $scope.entries.length; i--;) {
+      var user = $scope.entries[i];
+      // actually delete user
+      if (entry.isDeleted) {
+        $scope.entries.splice(i, 1);
+      }
+      // mark as not new
+      if (entry.isNew) {
+        entry.isNew = false;
+      }
+
+      // send on server
+      results.push($http.post('/saveEntry', entry));
+    }
+
+    return $q.all(results);
+  };
+}
 
 
 var services = angular.module('exampleApp.services', ['ngResource']);
